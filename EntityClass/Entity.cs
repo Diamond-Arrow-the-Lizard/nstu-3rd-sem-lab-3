5ff88fc2a8736f6e004ebdc3b33a12d71747501d
Diamond_Arrow <diamond1arrow@gmail.com>
2024-11-20 11:19:32 +0700

    Added fields to Entity class: HP, BaseATK, BaseDEF, isAlive

---
+++ EntityClass/Entity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace Entity;
+
+public class Entity  {
+    
+    public string Name {set; get; } = "Entity";
+
+    private int _hp = 1;
+    public int HP {
+        set {
+            if(value < 0) {
+                throw new Exception("ERROR: invalid HP value.");
+            }
+            else {
+                _hp = value;
+            }
+        } 
+        get => _hp;
+    }
+    
+    private int _atk = 1;
+    public int BaseATK {
+        set {
+            if(value <= 0) {
+                throw new Exception("ERROR: invalid ATK value.");
+            }
+            else {
+                _atk = value;
+            }
+        } 
+        get => _atk;
+    }
+
+    private int _def = 0;
+    public int BaseDEF {
+        set {
+            if(value < 0) {
+                throw new Exception("ERROR: invalid DEF value.");
+            }
+            else {
+                _def = value;
+            }
+        } 
+        get => _def;
+    }
+
+    public bool isAlive {set; get; } = true;
+
+
+}
